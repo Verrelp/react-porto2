1e518ed8acbbf0e366d9aea422c539c3ef7a0d41
verrelprayogii2@gmail.com <verrelprayogii2@gmail.com>
2023-11-04 10:15:20 +0800

    first commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,19 +1,60 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardItem from "./CardItem";
-import featuresData from "@/utils/data/projects.json";
+import projectsData from "@/utils/data/projects.json";
 
 const ProjectList = () => {
-  const [features, setFeatures] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedType, setSelectedType] = useState("");
 
   useEffect(() => {
-    setFeatures(featuresData);
+    setProjects(projectsData);
   }, []);
+
+  const filteredProjects = projects.filter((project) => {
+    const typeMatches = selectedType ? project.type.toLowerCase() === selectedType.toLowerCase() : true;
+    const searchMatches = project.name.toLowerCase().includes(searchQuery.toLowerCase());
+    return typeMatches && searchMatches;
+  });
 
   return (
     <section>
+      <div className="mb-3">
+        
+        <div className="btn-group" role="group" aria-label="Filter by type">
+          <button
+            type="button"
+            className={`btn ${selectedType === "nature" ? "btn-primary" : "btn-secondary"}`}
+            onClick={() => setSelectedType("nature")}
+          >
+            Nature
+          </button>
+          <button
+            type="button"
+            className={`btn ${selectedType === "modern" ? "btn-primary" : "btn-secondary"}`}
+            onClick={() => setSelectedType("modern")}
+          >
+            Modern
+          </button>
+          <button
+            type="button"
+            className={`btn ${selectedType === "classic" ? "btn-primary" : "btn-secondary"}`}
+            onClick={() => setSelectedType("classic")}
+          >
+            Classic
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => setSelectedType("")}
+          >
+            Clear Filter
+          </button>
+        </div>
+      </div>
       <hr />
       <div className="row">
-        {features.map((data, index) => {
+        {filteredProjects.map((data, index) => {
           return (
             <div className="col-md-3" key={index}>
               <CardItem
